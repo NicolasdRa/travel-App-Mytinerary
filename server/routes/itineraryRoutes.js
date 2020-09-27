7458da0c28600c051ecf2728aa7d6573253ec0d6
Nicolas <nicolasdirago@gmail.com>
2020-09-27 15:48:12 +0200

    dependecies updated

--- server/routes/itineraryRoutes.js
+++ server/routes/itineraryRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const auth = require('../middleware/authorization')
 const multer = require('multer')
 const {
   getAllItineraries,
