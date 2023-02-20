425bc64d26ee9de7bdebf3b80c009a65aea149e8
Nicolas <nicolasdirago@gmail.com>
2023-02-20 19:20:01 +0100

    fixed environment variables

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://localhost:3000/'
+export const baseUrl = process.env.REACT_APP_API_URL
 export const apiUrl = `${baseUrl}api/v1/`
 
 export const authUrl = `${apiUrl}auth/`
