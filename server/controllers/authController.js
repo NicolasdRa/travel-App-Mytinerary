426733523911ebc5d126e5f5f8ce875fbb45bffb
Nicolas di Rago <nicolasdirago@gmail.com>
2025-07-05 17:38:18 +0200

    cleanup

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,7 +8,6 @@ const crypto = require('crypto')
 const passport = require('passport')
 
 // AUTHENTICATION
-
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
